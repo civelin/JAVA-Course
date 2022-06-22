97016bf5b8dbd35a6bdf01fb9e74fd2b4c2c5d93
civelin <cvetelinaivanova7@gmail.com>
2022-06-22 23:52:03 +0300

    added methods for multiplication and for the menu

--- JAVA-Basics/finalProject/src/Main.java
+++ JAVA-Basics/finalProject/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int rows = 0;
         while (rows <= 0) {
-            System.out.println("Enter the number of rows of the matrix: ");
+            System.out.print("Enter the number of rows of the matrix: ");
             rows = scan.nextInt();
         }
         return rows;
@@ -16,7 +16,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int cols = 0;
         while (cols <= 0) {
-            System.out.println("Enter the number of columns of the matrix: ");
+            System.out.print("Enter the number of columns of the matrix: ");
             cols = scan.nextInt();
         }
         return cols;
@@ -29,14 +29,14 @@ public class Main {
         int[][] matrix = new int[numberOfRows][numberOfCols];
         for (int i = 0; i < numberOfRows; i++) {
             for (int j = 0; j < numberOfCols; j++) {
-                System.out.print("Enter element on index (" + i + ", " + j + "): ");
-                int currElem = scan.nextInt();
-                while(currElem != (int) currElem){
+                System.out.print("Enter integer on index (" + i + ", " + j + "): ");
+                double currElem = scan.nextDouble();
+                while (currElem != (int) currElem) {
                     System.out.println("You must enter an integer");
-                    System.out.print("Enter element on index (" + i + ", " + j + "): ");
-                    currElem = scan.nextInt();
+                    System.out.print("Enter integer on index (" + i + ", " + j + "): ");
+                    currElem = scan.nextDouble();
                 }
-                matrix[i][j] = currElem;
+                matrix[i][j] = (int) currElem;
             }
         }
         return matrix;
@@ -71,7 +71,7 @@ public class Main {
         }
     }
 
-    public static int[][] sumTwoMatrices(){
+    public static int[][] sumTwoMatrices() {
         int[][] matrix1 = enterMatrix();
         int[][] matrix2 = enterMatrix();
         int rowsMatrix1 = matrix1.length;
@@ -79,20 +79,20 @@ public class Main {
         int rowsMatrix2 = matrix2.length;
         int colsMatrix2 = matrix2[0].length;
         int[][] resultMatrix = new int[rowsMatrix1][colsMatrix1];
-        if(rowsMatrix1 == rowsMatrix2 && colsMatrix1 == colsMatrix2){
+        if (rowsMatrix1 == rowsMatrix2 && colsMatrix1 == colsMatrix2) {
             for (int i = 0; i < rowsMatrix1; i++) {
                 for (int j = 0; j < colsMatrix1; j++) {
                     resultMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
                 }
             }
-        } else{
+        } else {
             System.out.println("Invalid matrices' size. The size of the matrices must be equal.");
             return sumTwoMatrices();
         }
         return resultMatrix;
     }
 
-    public static int[][] subtractionTwoMatrices(){
+    public static int[][] subtractionTwoMatrices() {
         int[][] matrix1 = enterMatrix();
         int[][] matrix2 = enterMatrix();
         int rowsMatrix1 = matrix1.length;
@@ -100,13 +100,13 @@ public class Main {
         int rowsMatrix2 = matrix2.length;
         int colsMatrix2 = matrix2[0].length;
         int[][] resultMatrix = new int[rowsMatrix1][colsMatrix1];
-        if(rowsMatrix1 == rowsMatrix2 && colsMatrix1 == colsMatrix2){
+        if (rowsMatrix1 == rowsMatrix2 && colsMatrix1 == colsMatrix2) {
             for (int i = 0; i < rowsMatrix1; i++) {
                 for (int j = 0; j < colsMatrix1; j++) {
                     resultMatrix[i][j] = matrix1[i][j] - matrix2[i][j];
                 }
             }
-        } else{
+        } else {
             System.out.println("Invalid matrices' size. The size of the matrices must be equal.");
             return subtractionTwoMatrices();
         }
@@ -114,32 +114,76 @@ public class Main {
     }
 
 
-    public static void main(String[] args) {
+    public static int multiplyCell(int[][]matrix1, int[][]matrix2, int row, int col){
+        int cellResult = 0;
+        for (int i = 0; i < matrix2.length; i++) {
+            cellResult += matrix1[row][i] * matrix2[i][col];
+        }
+        return cellResult;
+    }
+
+    public static int[][] multiplyTwoMatrices(){
+        int[][] matrix1 = enterMatrix();
+        int[][] matrix2 = enterMatrix();
+        int rowsMatrix1 = matrix1.length;
+        int colsMatrix2 = matrix2[0].length;
+        while(rowsMatrix1 != colsMatrix2){
+            System.out.println("Invalid matrices' sizes. The number of rows of first matrix must be equal to number of columns of second matrix.");
+            matrix1 = enterMatrix();
+            matrix2 = enterMatrix();
+            rowsMatrix1 = matrix1.length;
+            colsMatrix2 = matrix2[0].length;
+        }
+        int[][] resultMatrix = new int[rowsMatrix1][colsMatrix2];
+
+        for(int i = 0; i < resultMatrix.length; i++){
+            for (int j = 0; j < resultMatrix[i].length; j++) {
+                resultMatrix[i][j] = multiplyCell(matrix1, matrix2, i, j);
+            }
+        }
+        return resultMatrix;
+    }
+
+    public static void menu() {
         Scanner scan = new Scanner(System.in);
         System.out.println("-> 1.Sum two matrices");
         System.out.println("-> 2.Subtraction of two matrices");
         System.out.println("-> 3.Multiplication of two matrices");
         System.out.println("-> 4.Find a determinant of a matrix");
         System.out.println("-> 5.Inverse a matrix");
+        System.out.println("-> Enter 0 to return");
         System.out.print("Choose an option from the menu (1-5) - ");
         int choice = scan.nextInt();
-
-        while(choice >= 6 || choice <= 0){
+        while (choice > 6 || choice <= 0) {
             System.out.println("Invalid choice!");
-            System.out.print("Choose an option from the menu (1-5) - ");
+            System.out.print("Choose an option from the menu (1-6) - ");
             choice = scan.nextInt();
         }
-        switch (choice){
+        switch (choice) {
+            case 0:
+                return;
             case 1:
-                int[][] sumMatrix = sumTwoMatrices(); printMatrix(sumMatrix); break;
+                int[][] sumMatrix = sumTwoMatrices();
+                printMatrix(sumMatrix);
+                break;
             case 2:
-                int[][] subsMatrix = subtractionTwoMatrices(); printMatrix(subsMatrix);break;
-            case 3: break;
-            case 4: break;
-            case 5: break;
+                int[][] subsMatrix = subtractionTwoMatrices();
+                printMatrix(subsMatrix);
+                break;
+            case 3:
+                int[][] multiplicatedMatrix = multiplyTwoMatrices();
+                printMatrix(multiplicatedMatrix);
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
         }
+    }
 
 
+    public static void main(String[] args) {
+        menu();
 
     }
 }
