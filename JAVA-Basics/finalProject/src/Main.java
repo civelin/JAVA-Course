8c6ebcf6fd9cd9ba3e5268ffe738665d44a07a2a
civelin <cvetelinaivanova7@gmail.com>
2022-07-09 00:31:12 +0300

    added method canMatrixBeReducedToIdentityMatrix

--- JAVA-Basics/finalProject/src/Main.java
+++ JAVA-Basics/finalProject/src/Main.java
@@ -22,34 +22,35 @@ public class Main {
                 int input = Integer.parseInt(scan.nextLine());
                 if (input <= 0) {
                     System.out.println("Invalid input. The size must be a positive integer number");
+                    System.out.print("Enter the size again - ");
                     continue;
                 }
                 return input;
             } catch (Exception e) {
                 System.out.println("Invalid input. The size must be a positive integer number");
-                continue;
+                System.out.print("Enter the size again - ");
             }
         }
     }
 
-    private static int validateMatrixElements(Scanner scan) {
+    private static double validateMatrixElements(Scanner scan) {
         while (true) {
             try {
-                int input = Integer.parseInt(scan.nextLine());
+                double input = Double.parseDouble(scan.nextLine());
                 return input;
             } catch (Exception e) {
-                System.out.println("Invalid input. The element must be a integer number");
-                continue;
+                System.out.println("Invalid input. The element must be a number");
             }
         }
     }
 
 
-    public static int[][] enterMatrix() {
+    public static double[][] enterMatrix() {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a matrix: ");
         int numberOfRows = enterNumberOfRows();
         int numberOfCols = enterNumberOfCols();
-        int[][] matrix = new int[numberOfRows][numberOfCols];
+        double[][] matrix = new double[numberOfRows][numberOfCols];
         for (int i = 0; i < numberOfRows; i++) {
             for (int j = 0; j < numberOfCols; j++) {
                 System.out.print("Enter integer on index (" + i + ", " + j + "): ");
@@ -59,7 +60,7 @@ public class Main {
         return matrix;
     }
 
-    public static int[] getTheLengthOfTheLongestElemInEachColumn(int[][] matrix) {
+    public static int[] getTheLengthOfTheLongestElemInEachColumn(double[][] matrix) {
         int rows = matrix.length;
         int cols = matrix[0].length;
         int maxElemLength = 0;
@@ -77,7 +78,7 @@ public class Main {
         return registerOfTheLongestElemInEachColumn;
     }
 
-    public static void printMatrix(int[][] matrix) {
+    public static void printMatrix(double[][] matrix) {
         int[] register = getTheLengthOfTheLongestElemInEachColumn(matrix);
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
@@ -88,15 +89,21 @@ public class Main {
         }
     }
 
-    public static int[][] sumTwoMatrices() {
-        int[][] matrix1 = enterMatrix();
-        int[][] matrix2 = enterMatrix();
+    public static boolean haveEqualSizes(double[][] matrix1, double[][] matrix2) {
+        int matrix1Rows = matrix1.length;
+        int matrix1Cols = matrix1[0].length;
+        int matrix2Rows = matrix2.length;
+        int matrix2Cols = matrix2[0].length;
+        return (matrix1Rows == matrix2Rows && matrix1Cols == matrix2Cols);
+    }
+
+    public static double[][] sumTwoMatrices() {
+        double[][] matrix1 = enterMatrix();
+        double[][] matrix2 = enterMatrix();
         int rowsMatrix1 = matrix1.length;
         int colsMatrix1 = matrix1[0].length;
-        int rowsMatrix2 = matrix2.length;
-        int colsMatrix2 = matrix2[0].length;
-        int[][] resultMatrix = new int[rowsMatrix1][colsMatrix1];
-        if (rowsMatrix1 != rowsMatrix2 || colsMatrix1 != colsMatrix2) {
+        double[][] resultMatrix = new double[rowsMatrix1][colsMatrix1];
+        if (haveEqualSizes(matrix1, matrix2)) {
             System.out.println("Invalid matrices' size. The size of the matrices must be equal.");
             return sumTwoMatrices();
         } else {
@@ -109,15 +116,13 @@ public class Main {
         return resultMatrix;
     }
 
-    public static int[][] subtractionTwoMatrices() {
-        int[][] matrix1 = enterMatrix();
-        int[][] matrix2 = enterMatrix();
+    public static double[][] subtractionTwoMatrices() {
+        double[][] matrix1 = enterMatrix();
+        double[][] matrix2 = enterMatrix();
         int rowsMatrix1 = matrix1.length;
         int colsMatrix1 = matrix1[0].length;
-        int rowsMatrix2 = matrix2.length;
-        int colsMatrix2 = matrix2[0].length;
-        int[][] resultMatrix = new int[rowsMatrix1][colsMatrix1];
-        if (rowsMatrix1 == rowsMatrix2 && colsMatrix1 == colsMatrix2) {
+        double[][] resultMatrix = new double[rowsMatrix1][colsMatrix1];
+        if (haveEqualSizes(matrix1, matrix2)) {
             for (int i = 0; i < rowsMatrix1; i++) {
                 for (int j = 0; j < colsMatrix1; j++) {
                     resultMatrix[i][j] = matrix1[i][j] - matrix2[i][j];
@@ -131,7 +136,7 @@ public class Main {
     }
 
 
-    public static int multiplyCell(int[][] matrix1, int[][] matrix2, int row, int col) {
+    public static int multiplyCell(double[][] matrix1, double[][] matrix2, int row, int col) {
         int cellResult = 0;
         for (int i = 0; i < matrix2.length; i++) {
             cellResult += matrix1[row][i] * matrix2[i][col];
@@ -139,13 +144,13 @@ public class Main {
         return cellResult;
     }
 
-    public static int[][] multiplyTwoMatrices() {
-        int[][] matrix1 = enterMatrix();
-        int[][] matrix2 = enterMatrix();
+    public static double[][] multiplyTwoMatrices() {
+        double[][] matrix1 = enterMatrix();
+        double[][] matrix2 = enterMatrix();
         int rowsMatrix1 = matrix1.length;
         int colsMatrix2 = matrix2[0].length;
         if (rowsMatrix1 == colsMatrix2) {
-            int[][] resultMatrix = new int[rowsMatrix1][colsMatrix2];
+            double[][] resultMatrix = new double[rowsMatrix1][colsMatrix2];
             for (int i = 0; i < resultMatrix.length; i++) {
                 for (int j = 0; j < resultMatrix[i].length; j++) {
                     resultMatrix[i][j] = multiplyCell(matrix1, matrix2, i, j);
@@ -158,9 +163,9 @@ public class Main {
         }
     }
 
-    public static int[][] findCofactorMatrix(int[][] matrix, int row, int col) {
+    public static double[][] findCofactorMatrix(double[][] matrix, int row, int col) {
         int length = matrix.length;
-        int[][] newMatrix = new int[length - 1][length - 1];
+        double[][] newMatrix = new double[length - 1][length - 1];
         int newMatrixCurrRow = 0;
         int newMatrixCurrCol = 0;
         for (int i = 0; i < length; i++) {
@@ -178,8 +183,8 @@ public class Main {
         return newMatrix;
     }
 
-    public static int findDeterminant(int[][] matrix) {
-        int det = 0;
+    public static double findDeterminant(double[][] matrix) {
+        double det = 0;
         int length = matrix.length;
         if (length == 1) {
             det = matrix[0][0];
@@ -187,16 +192,16 @@ public class Main {
         }
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
-                int[][] cofactorMatrix = findCofactorMatrix(matrix, 0, j);
+                double[][] cofactorMatrix = findCofactorMatrix(matrix, 0, j);
                 det += Math.pow(-1, (i + j)) * matrix[i][j] * findDeterminant(cofactorMatrix);
             }
         }
         return det;
     }
 
-    public static int[][] findTransposeMatrix(int[][] matrix) {
+    public static double[][] findTransposeMatrix(double[][] matrix) {
         int length = matrix.length;
-        int[][] transposeMatrix = new int[length][length];
+        double[][] transposeMatrix = new double[length][length];
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
                 transposeMatrix[i][j] = matrix[j][i];
@@ -206,9 +211,9 @@ public class Main {
     }
 
     public static double[][] findInverseMatrix() {
-        int[][] matrix = enterMatrix();
+        double[][] matrix = enterMatrix();
         if (matrix.length == matrix[0].length) {
-            int det = findDeterminant(matrix);
+            double det = findDeterminant(matrix);
             if (det == 0) {
                 System.out.println("The determinant is equal to 0. The matrix has no equal matrix");
                 System.out.println("Enter another matrix.");
@@ -217,9 +222,9 @@ public class Main {
                 double[][] inverseMatrix = new double[matrix.length][matrix[0].length];
                 for (int i = 0; i < matrix.length; i++) {
                     for (int j = 0; j < matrix[0].length; j++) {
-                        int[][] transposeMatrix = findTransposeMatrix(matrix);
-                        int[][] cofactorMatrix = findCofactorMatrix(transposeMatrix, i, j);
-                        int cofactorMatrixDet = findDeterminant(cofactorMatrix);
+                        double[][] transposeMatrix = findTransposeMatrix(matrix);
+                        double[][] cofactorMatrix = findCofactorMatrix(transposeMatrix, i, j);
+                        double cofactorMatrixDet = findDeterminant(cofactorMatrix);
                         inverseMatrix[i][j] = cofactorMatrixDet * Math.pow(-1, (i + j)) * 1 / det;
                     }
                 }
@@ -231,49 +236,56 @@ public class Main {
         }
     }
 
+    public static void canMatrixBeReducedToIdentityMatrix() {
+        double[][] matrix = enterMatrix();
+        double det = findDeterminant(matrix);
+        if (det != 0) {
+            System.out.println("The matrix can be reduced to the identity matrix");
+        } else {
+            System.out.println("The matrix cannot be reduced to the identity matrix");
+        }
+    }
+
     public static void chooseFromMenu() {
         Scanner scan = new Scanner(System.in);
         int choice = scan.nextInt();
 
         switch (choice) {
-            case 0:
-                return;
             case 1:
                 System.out.println();
-                int[][] sumMatrix = sumTwoMatrices();
+                double[][] sumMatrix = sumTwoMatrices();
                 printMatrix(sumMatrix);
                 break;
             case 2:
                 System.out.println();
-                int[][] subsMatrix = subtractionTwoMatrices();
+                double[][] subsMatrix = subtractionTwoMatrices();
                 printMatrix(subsMatrix);
                 break;
             case 3:
                 System.out.println();
-                int[][] resultMatrix = multiplyTwoMatrices();
+                double[][] resultMatrix = multiplyTwoMatrices();
                 printMatrix(resultMatrix);
                 break;
             case 4:
                 System.out.println();
-                int[][] matrix = enterMatrix();
+                double[][] matrix = enterMatrix();
                 while (matrix.length != matrix[0].length) {
                     System.out.println("The matrix must be a square one");
                     matrix = enterMatrix();
                 }
-                int determinant = findDeterminant(matrix);
+                double determinant = findDeterminant(matrix);
                 System.out.println("Determinant of the entered matrix is " + determinant);
                 break;
             case 5:
                 double[][] inverseMatrix = findInverseMatrix();
-                for (int i = 0; i < inverseMatrix.length; i++) {
-                    for (int j = 0; j < inverseMatrix[0].length; j++) {
-                        System.out.println(inverseMatrix[i][j]);
-                    }
-                }
+                printMatrix(inverseMatrix);
                 break;
+            case 6:
+                 canMatrixBeReducedToIdentityMatrix();
+                 break;
             default:
                 System.out.println("Invalid choice!");
-                System.out.print("Choose an option from the menu (1-5) - ");
+                System.out.print("Choose an option from 1 to 6 - ");
                 chooseFromMenu();
                 break;
         }
@@ -286,8 +298,8 @@ public class Main {
         System.out.println("---> 3.Multiplication of two matrices");
         System.out.println("---> 4.Find a determinant of a matrix");
         System.out.println("---> 5.Inverse a matrix");
-        System.out.println("---> Enter 0 to return");
-        System.out.print("Choose an option from the menu (1-5) - ");
+        System.out.println("---> 6. Check if a matrix can be reduced to the identity matrix");
+        System.out.print("Choose an option from 1 to 6 - ");
         chooseFromMenu();
 
     }
